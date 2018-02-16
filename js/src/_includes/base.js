d045a577178afb9530185dba481e3efc6f942af0
Gabe Nuñez <gabe@gabenunez.com>
2018-02-16 12:36:28 -0800

    Added a fallback for Safari

--- js/src/_includes/base.js
+++ js/src/_includes/base.js
@@ -1,8 +1,23 @@
 var RickHead = SVG.get('#head');
 var efNoSign = SVG.get("#ef-no");
 
-RickHead.click(function() { 
-    this.animate(500).scale(0).after(function() {
+// Checks if the browser is Safari
+var is_safari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+RickHead.click(function() {
+    if (is_safari) {
+
+        // Safari: Just hide and show for because it doesn't
+        // seem to support svgjs too well.
+        this.hide();
         efNoSign.show();
-    });
+
+    } else {
+
+        // All Other Browsers: scale, then show the sign.
+        this.animate(500).scale(0, 0).after(function() {
+            efNoSign.show();
+            
+        });
+    }
 });
