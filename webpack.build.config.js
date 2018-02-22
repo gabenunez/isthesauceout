a0998cf3fd6f9a34aaf09de9ba9b37ac2faae618
Gabe Nuñez <gabe@gabenunez.com>
2018-02-21 21:35:35 -0800

    Added npm build command

---
+++ webpack.build.config.js
@@ -0,0 +1,34 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    bundle:["./js/src/dependencies.js", "./js/src/app.js"], 
+    main:["./css/src/base.css"]
+  },
+  output: {
+    path: path.join(__dirname, '/_site/'),
+    filename: './js/[name].js',
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("./css/main.css"),
+    new HtmlWebpackPlugin({
+        hash: true,
+        filename: 'index.html',
+        template: __dirname + '/index.html',
+    }),
+  ]
+}
